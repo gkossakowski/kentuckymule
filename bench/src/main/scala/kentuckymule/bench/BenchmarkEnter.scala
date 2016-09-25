d54ff61d224f69c98e16cf3f2bd390c755863093
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2016-09-25 23:48:18 +0200

    Add benchmark for entering symbols.
    The benchmark shows we can populate the symbol table with declarations
    from Typer.scala at the rate of 85k ops per second.

---
+++ bench/src/main/scala/kentuckymule/bench/BenchmarkEnter.scala
@@ -0,0 +1,58 @@
+package kentuckymule.bench
+
+import dotty.tools.dotc.core.Contexts.{Context, ContextBase}
+import dotty.tools.dotc.core.Enter
+import dotty.tools.dotc.{CompilationUnit, parsing}
+import dotty.tools.dotc.util.{NoSource, SourceFile}
+import org.openjdk.jmh.annotations._
+
+import scala.reflect.io.PlainFile
+
+
+object BenchmarkEnter {
+
+  @State(Scope.Benchmark)
+  class BenchmarkState {
+    private val initCtx = (new ContextBase).initialCtx
+    val context = initCtx.fresh
+  }
+
+  @State(Scope.Thread)
+  class ThreadState {
+    @Param(Array("sample-files/Typer.scala.ignore"))
+    var filePath: String = _
+    var source: SourceFile = _
+    var compilationUnit: CompilationUnit = _
+    @Setup(Level.Trial)
+    def createSource(bs: BenchmarkState): Unit = {
+      source = getSource(filePath)(bs.context)
+      compilationUnit = new CompilationUnit(source)
+      val parser = new parsing.Parsers.Parser(source)(bs.context)
+      compilationUnit.untpdTree = parser.parse()
+    }
+  }
+
+  def getSource(fileName: String)(ctx: Context): SourceFile = {
+    val f = new PlainFile(fileName)
+    if (f.exists) new SourceFile(f)
+    else {
+      ctx.error(s"not found: $fileName")
+      NoSource
+    }
+  }
+
+}
+
+class BenchmarkEnter {
+  import BenchmarkEnter._
+  @Benchmark
+  @Warmup(iterations = 20)
+  @Measurement(iterations = 20)
+  @Fork(3)
+  def enter(bs: BenchmarkState, ts: ThreadState): Unit = {
+    val context = bs.context
+    context.definitions.rootPackage.clear()
+    val enter = new Enter
+    enter.enterCompilationUnit(ts.compilationUnit)(context)
+  }
+}
