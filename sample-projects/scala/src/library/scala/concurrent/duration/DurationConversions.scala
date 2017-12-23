accfaf8c1f3d5ddcae0404af46e3424acef0b505
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2017-12-23 18:02:30 +0100

    Comment out methods with method-dependent types in Duration

--- sample-projects/scala/src/library/scala/concurrent/duration/DurationConversions.scala
+++ sample-projects/scala/src/library/scala/concurrent/duration/DurationConversions.scala
@@ -42,32 +42,32 @@ trait DurationConversions extends Any {
   def days         = durationIn(DAYS)
   def day          = days
 
-  def nanoseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(nanoseconds)
-  def nanos[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
-  def nanosecond[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
-  def nano[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
+  // def nanoseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(nanoseconds)
+  // def nanos[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
+  // def nanosecond[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
+  // def nano[C](c: C)(implicit ev: Classifier[C]): ev.R = nanoseconds(c)
 
-  def microseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(microseconds)
-  def micros[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
-  def microsecond[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
-  def micro[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
+  // def microseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(microseconds)
+  // def micros[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
+  // def microsecond[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
+  // def micro[C](c: C)(implicit ev: Classifier[C]): ev.R = microseconds(c)
 
-  def milliseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(milliseconds)
-  def millis[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
-  def millisecond[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
-  def milli[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
+  // def milliseconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(milliseconds)
+  // def millis[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
+  // def millisecond[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
+  // def milli[C](c: C)(implicit ev: Classifier[C]): ev.R = milliseconds(c)
 
-  def seconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(seconds)
-  def second[C](c: C)(implicit ev: Classifier[C]): ev.R = seconds(c)
+  // def seconds[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(seconds)
+  // def second[C](c: C)(implicit ev: Classifier[C]): ev.R = seconds(c)
 
-  def minutes[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(minutes)
-  def minute[C](c: C)(implicit ev: Classifier[C]): ev.R = minutes(c)
+  // def minutes[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(minutes)
+  // def minute[C](c: C)(implicit ev: Classifier[C]): ev.R = minutes(c)
 
-  def hours[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(hours)
-  def hour[C](c: C)(implicit ev: Classifier[C]): ev.R = hours(c)
+  // def hours[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(hours)
+  // def hour[C](c: C)(implicit ev: Classifier[C]): ev.R = hours(c)
 
-  def days[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(days)
-  def day[C](c: C)(implicit ev: Classifier[C]): ev.R = days(c)
+  // def days[C](c: C)(implicit ev: Classifier[C]): ev.R = ev.convert(days)
+  // def day[C](c: C)(implicit ev: Classifier[C]): ev.R = days(c)
 }
 
 /**
